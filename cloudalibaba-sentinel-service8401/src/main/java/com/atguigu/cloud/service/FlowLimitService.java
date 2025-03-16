12317204349f425c215eb606058e08b510653a02
zhangsan <zhangsan@gmail.com>
2025-03-16 19:32:02 +0800

    new create cloud2025 project

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/service/FlowLimitService.java
@@ -0,0 +1,18 @@
+package com.atguigu.cloud.service;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Service
+public class FlowLimitService
+{
+    @SentinelResource(value = "common")
+    public void common()
+    {
+        System.out.println("------FlowLimitService come in");
+    }
+
+
+}
